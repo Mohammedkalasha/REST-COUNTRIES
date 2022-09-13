c81f34010e528d8e5241d8a10fd2934963fa5239
Mohammedkalasha <109412459+Mohammedkalasha@users.noreply.github.com>
2022-09-14 03:20:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function foo(value,value1){
         row.innerHTML+=`
         <div class="col-md-4">
         <div class="card border-light mb-3" style="max-width: 18rem; text-align:center; background-color:#F8F9FA">
-        <div class="card-header" style="background-color:">"${value[i].name}"</div>
+        <div class="card-header" style="background-color:">${value[i].name}</div>
         <img src="${value[i].flag}" class="card-img-top" style="padding:10px"><br>
        
-        <p>capital:"${value[i].capital}"</p>
-        <p>region:"${value[i].region}"</p>
-        <p>country code:"${value[i].cioc}"</p>
+        <p>capital:${value[i].capital}</p>
+        <p>region:${value[i].region}</p>
+        <p>country code:${value[i].cioc}</p>
         <button type="button" class="btn btn-primary" style="margin:20px">click for weather</button>
         
        
